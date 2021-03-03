332b6417deb3d9b7713f03ea476845f40be7b239
Manish <man4uonly@gmail.com>
2021-03-03 22:29:34 +0530

    ui layout styling

--- GloboTicket/globoTicketUI/src/app/app-routing.module.ts
+++ GloboTicket/globoTicketUI/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConferenceComponent } from './conference/conference.component';
+import { LayoutComponent } from './common/layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'conferences', component: ConferenceComponent }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent, pathMatch: 'full'},
+      { path: 'conferences', component: ConferenceComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- GloboTicket/globoTicketUI/src/app/app.module.ts
+++ GloboTicket/globoTicketUI/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ConferenceComponent } from './conference/conference.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LayoutComponent } from './common/layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConferenceComponent
+    ConferenceComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
